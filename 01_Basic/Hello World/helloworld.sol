7b29f014c56d70079e927eec692e828b3df9478f
Jason Victor <jason_victor@outlook.com>
2025-06-23 17:38:12 -0400

    Updated source code file for hello world

--- 01_Basic/Hello World/helloworld.sol
+++ 01_Basic/Hello World/helloworld.sol
@@ -5,14 +5,12 @@ pragma solidity ^0.8.26;
 // 🛠️ Tells the builder (compiler) which tools to use.
 // Solidity version 0.8.26 ensures we're using a version that avoids bugs and supports new features.
 
+/// @title HelloWorld Smart Contract
+/// @author [Your Name]
+/// @notice A basic smart contract that stores and exposes a greeting message
+/// @dev Think of this as a digital building with a welcome sign that everyone can read, but no one can change.
 contract HelloWorld {
-    // 🏢 Think of this as creating a small digital building called "HelloWorld".
-    // This building can store info and offer services (functions) to the public.
-
+    /// @notice A publicly visible greeting message stored on the blockchain
+    /// @dev Acts like a permanent welcome sign on the front door that says "Hello World!"
     string public greet = "Hello World!";
-    // 🪧 This is like a welcome sign posted on the front of the building.
-    // It's a permanent sign (state variable) that says "Hello World!".
-    // The keyword 'public' makes this sign visible to anyone who walks by (i.e., any external user).
-    // Solidity automatically installs a window so people can *look at* the sign (getter function), but they can't change it unless given permission.
 }
-
